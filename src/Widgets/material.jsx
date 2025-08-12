1074821ead4ee4c8dca3f32f90ffdefb8f3f4e38
AndresOzil10 <andresref29@gmail.com>
2025-08-12 10:22:10 -0600

    Update sintaxis

--- src/Widgets/material.jsx
+++ src/Widgets/material.jsx
@@ -1,7 +1,19 @@
 import { useState } from "react"
+import CloseIcon from "../icons/close"
+import SaveIcon from "../icons/saveIcon"
+import user from "../assets/gif/user.gif"
+import gif from "../assets/gif/password.gif"
 
 const Material = () => { 
   const [Login, setLogin] = useState(false)
+  const [Usuario, setUsuario] = useState('')
+  const [Password, setPassword] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const agregarRegistro = async () => {
+        setIsLoading(true);
+        
+    }
     return (
         <>
             <div className="overflow-x-auto shadow-2xl">
@@ -54,13 +66,53 @@ const Material = () => {
           </table>
           <dialog open={Login} className="modal">
             <div className="modal-box">
-              <h3 className="font-bold text-lg">Hello!</h3>
-              <p className="py-4">Press ESC key or click the button below to close</p>
-              <div className="modal-action">
-                <form method="dialog">
-                  {/* if there is a button in form, it will close the modal */}
-                  <button className="btn" onClick={()=>setLogin(false)}>Close</button>
-                </form>
+              <h3 className="font-bold text-lg text-center">Log In!</h3>
+                <div className="w-full flex flex-col items-center mt-3">
+                  <label className="input validator">
+                    <img src={user} alt="" width={20} height={20}/>
+                    <input
+                      type="input"
+                      required
+                      placeholder="Username"
+                      pattern="[A-Za-z][A-Za-z0-9\-]*"
+                      minLength="3"
+                      maxLength="30"
+                      title="Only letters, numbers or dash"
+                      value={Usuario}
+                      onChange={e => setUsuario(e.target.value)}
+                    />
+                  </label>
+                  <p className="validator-hint">
+                  Complet the username
+                  </p>
+              </div>
+              <div className="w-full flex flex-col items-center">
+                <label className="input validator">
+                  <img src={gif} alt="" width={20} height={20}/>
+                  <input
+                    type="password"
+                    required
+                    placeholder="Password"
+                    minLength="8"
+                    value={Password}
+                    onChange={e => setPassword(e.target.value)}
+                  />
+                </label>
+                  <p className="validator-hint hidden">
+                  Complet the password
+                  </p>
+              </div>
+              <div className="modal-action ml-9 ">
+                <button
+                  className="btn bg-transparent border-none shadow-none"
+                  onClick={agregarRegistro}
+                  disabled={isLoading}
+                >
+                  {isLoading ? <span className="loading loading-infinity text-info"></span> : "Login"}
+                </button>
+                <button className="btn bg-transparent border-none shadow-none" onClick={() => setLogin(false)}>
+                  <CloseIcon />
+                </button>
               </div>
             </div>
           </dialog>
